2fd955832099a2764d1d4c14fb076de106014db1
rifkifrds <muhamadrifkifirdaus22@gmail.com>
2026-07-01 00:15:33 +0700

    first commit

--- app/(auth)/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="mx-auto min-h-screen max-w-md bg-neutral-50 px-6 py-12">
-      {children}
-    </div>
-  );
-}
